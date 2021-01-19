74081832278f97f4646987b4e56b162cb3586308
Juan Pablo Carreon Hidalgo <jpch_26@outlook.com>
2021-01-19 12:26:41 -0600

    Main Script

---
+++ Main.R
@@ -0,0 +1,16 @@
+
+# Main Script -------------------------------------------------------------
+
+# Preferably, before you run this script restart your R session: 
+# Ctrl+Shift+F10 if you are using RStudio
+
+# Packages
+
+library(dplyr)   # To summarize data
+library(ggplot2) # To make graphs
+library(purrr)   # Iteration issues
+library(stringi) # To assign random strings on data simulation
+
+# 1 Simulating the data
+
+source("data/data_simulation.R")
