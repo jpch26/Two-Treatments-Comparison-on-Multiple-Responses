159087ff06ef6f32d317ef6ab13177250a41c692
Juan Pablo Carreon Hidalgo <jpch_26@outlook.com>
2021-01-25 21:27:45 -0600

    Erase all data and graphs outputs from analysis scripts

---
+++ erase_all_ouputs.R
@@ -0,0 +1,18 @@
+# Erase all outputs from analysis and data simulation ---------------------
+
+# Run this code if you want to erase all result files from analysis and data
+# simulation. 
+
+# Presentation documents and session info file names
+## document_and_session <- c()
+
+# Data outputs file names from analysis scripts
+analysis_outputs <- list.files("data")
+
+# Graph outputs file names from analysis scripts
+graph_outputs <- list.files("graphs")
+
+# Delete the specified files 
+## file.remove(document_and_session)
+file.remove(paste0("data/", analysis_outputs))
+file.remove(paste0("graphs/", graph_outputs))
